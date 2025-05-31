3968c7f8f39f4ccbee26f136dfe24b2d71332402
ca1e <i@scp500.com>
2025-06-01 00:42:45 +0800

    [update]docs

--- EC.Script/Text/TextLine.cs
+++
@@ -1,8 +0,0 @@
-namespace EC.Script.Text;
-
-public sealed class TextLine
-{
-    public string Text { get; init; } = string.Empty;
-    public string Comment { get; init; } = string.Empty;
-    public uint Line { get; init; } = 0;
-}
